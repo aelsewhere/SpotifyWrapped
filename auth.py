db903719cb861ead1f2a5552e1f91c0f7e1d0db4
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2024-12-19 15:21:37 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,19 +6,19 @@ from spotipy import util
 from secret import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 def authenticate():
-    scope = 'user-library-read'
-    token = util.prompt_for_user_token('user', scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
-    if token:
-        return spotipy.Spotify(auth=token)
-    else:
-        print("Can't get token for", 'user')
-        return None
+  scope = 'user-library-read'
+  token = util.prompt_for_user_token('user', scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
+  if token:
+    return spotipy.Spotify(auth=token)
+  else:
+    print("Can't get token for", 'user')
+    return None
     
 if __name__ == '__main__':
-    sp = authenticate()
-    if sp:
-        print("Successfully authenticated")
-    else:
-        print("Failed to authenticate")
+  sp = authenticate()
+  if sp:
+    print("Successfully authenticated")
+  else:
+    print("Failed to authenticate")
 
 
