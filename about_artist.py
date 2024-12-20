f4b38f777728a58e5817be2da74733b4a387de58
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2024-12-20 15:15:07 -0500

    Update about_artist.py

--- about_artist.py
+++ about_artist.py
@@ -5,6 +5,7 @@
 import spotipy
 from spotipy import util
 from user_info import get_user
+from user_prompts import prompt_user_artist_URI
 
 def print_albums():
   albums = get_albums(get_user())
@@ -12,20 +13,13 @@ def print_albums():
     print(album['name'])
 
 def get_albums(sp):
-  artist_uri = prompt_user()
+  artist_uri = prompt_user_artist_URI()
   results = sp.artist_albums(artist_uri, album_type='album')
   albums = results['items']
   while results['next']:
     results = sp.next(results)
     albums.extend(results['items'])
   return albums
-
-
-def prompt_user():  
-  artist_uri = input("Enter the artist URI: ")
-  if not artist_uri:
-    artist_uri = input("Enter the artist URI: ")
-  return artist_uri
 
 
 if __name__ == "__main__":
